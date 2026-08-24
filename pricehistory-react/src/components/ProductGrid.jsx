e9f1885252c4777a296ff000d828fe352de07922
raghvanpopli <raghvanpopli1313@gmail.com>
2026-08-24 16:32:17 +0530

    ProductGrid.jsx

--- pricehistory-react/src/components/ProductGrid.jsx
+++ pricehistory-react/src/components/ProductGrid.jsx
@@ -1,11 +1,5 @@
 import ProductCard from "./ProductCard";
 
-/**
- * ProductGrid - renders a list of ProductCard components.
- * Props:
- *   - products: array of product objects
- *   - emptyMessage: text shown when the list is empty
- */
 function ProductGrid({ products, emptyMessage = "No products found." }) {
   if (!products.length) {
     return <p className="empty-message">{emptyMessage}</p>;
